4402e1034be90d7317596e0f60e62e511f5f36ee
Alexey Shmalko <rasen.dubi@gmail.com>
2016-02-04 18:19:24 +0200

    Factor out cursor movement, normalize cursor position

--- edit-kit/EditKit/Editor.hs
+++ edit-kit/EditKit/Editor.hs
@@ -10,14 +10,16 @@ module EditKit.Editor
     , feedInput
     ) where
 
-import EditKit.Buffer (Buffer)
+import EditKit.Buffer (Buffer(Buffer), Cursor(Cursor))
 import qualified EditKit.Buffer as Buffer
 import EditKit.Input
 
-import Data.Text (Text)
-import STMContainers.Map (Map)
-import Control.Monad (when, void)
 import Control.Concurrent.STM (STM, TVar, newTVar, newTVarIO, writeTVar, readTVar, atomically)
+import Control.Monad (when, void)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import STMContainers.Map (Map)
 import qualified STMContainers.Map as Map
 
 data Editor = Editor
@@ -61,37 +63,33 @@ feedInput editor (EventKey (KeyChar 'l') []) = atomically $ moveRight editor
 feedInput _ _ = return ()
 
 moveUp :: Editor -> STM ()
-moveUp editor = do
-  void $ withCurrentBuffer editor $ \tbuffer -> do
-    buffer <- readTVar tbuffer
-    let c  = Buffer.cursor buffer
-    let c' = c{ Buffer.cursorLine = Buffer.cursorLine c - 1 }
-    when (Buffer.isValidCursor buffer c') $
-      writeTVar tbuffer buffer{ Buffer.cursor = c' }
+moveUp = moveCursor $ \c -> c{ Buffer.cursorLine = Buffer.cursorLine c - 1 }
 
 moveDown :: Editor -> STM ()
-moveDown editor = do
-  void $ withCurrentBuffer editor $ \tbuffer -> do
-    buffer <- readTVar tbuffer
-    let c  = Buffer.cursor buffer
-    let c' = c{ Buffer.cursorLine = Buffer.cursorLine c + 1 }
-    when (Buffer.isValidCursor buffer c') $
-      writeTVar tbuffer buffer{ Buffer.cursor = c' }
+moveDown = moveCursor $ \c -> c{ Buffer.cursorLine = Buffer.cursorLine c + 1 }
 
 moveLeft :: Editor -> STM ()
-moveLeft editor = do
-  void $ withCurrentBuffer editor $ \tbuffer -> do
-    buffer <- readTVar tbuffer
-    let c  = Buffer.cursor buffer
-    let c' = c{ Buffer.cursorColumn = Buffer.cursorColumn c - 1 }
-    when (Buffer.isValidCursor buffer c') $
-      writeTVar tbuffer buffer{ Buffer.cursor = c' }
+moveLeft = moveCursor $ \c -> c{ Buffer.cursorColumn = Buffer.cursorColumn c - 1 }
 
 moveRight :: Editor -> STM ()
-moveRight editor = do
+moveRight = moveCursor $ \c -> c{ Buffer.cursorColumn = Buffer.cursorColumn c + 1 }
+
+moveCursor :: (Cursor -> Cursor) -> Editor -> STM ()
+moveCursor f editor = do
   void $ withCurrentBuffer editor $ \tbuffer -> do
     buffer <- readTVar tbuffer
-    let c  = Buffer.cursor buffer
-    let c' = c{ Buffer.cursorColumn = Buffer.cursorColumn c + 1 }
-    when (Buffer.isValidCursor buffer c') $
-      writeTVar tbuffer buffer{ Buffer.cursor = c' }
+    let c = normalizeCursor buffer (f $ Buffer.cursor buffer)
+    when (Buffer.isValidCursor buffer c) $
+      writeTVar tbuffer buffer{ Buffer.cursor = c }
+
+normalizeCursor :: Buffer -> Cursor -> Cursor
+normalizeCursor Buffer{..} Cursor{..} = Cursor nLine nColumn
+  where
+    nLines = V.length lines
+    nLine | cursorLine < 0        = 0
+          | cursorLine >= nLines  = nLines - 1
+          | otherwise             = cursorLine
+    lineLen = if nLine < nLines then T.length (lines V.! nLine) else 0
+    nColumn | cursorColumn < 0         = 0
+            | cursorColumn >= lineLen  = lineLen - 1
+            | otherwise                = cursorColumn
